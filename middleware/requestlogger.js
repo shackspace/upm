ec5e76502e0aae6ecde134a102970eebab9bc91a
Sven Pfleiderer <sg+github@roothausen.de>
2011-05-22 15:36:06 +0200

    Add first prototype of requestlogger

---
+++ middleware/requestlogger.js
@@ -0,0 +1,21 @@
+var Util = require('util');
+
+var defaultLogger = {
+  debug: console.log
+};
+
+module.exports = function (log) {
+  log = log || defaultLogger; 
+
+  return function (req, res, next) {
+    var data = {
+      url: req.url,
+      headers: req.headers,
+      body: req.rawBody
+    };
+
+    log.debug(Util.inspect(data));
+
+    next();
+  };
+};
