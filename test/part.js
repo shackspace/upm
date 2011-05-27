2d3921c4647da14c7c0cc2bc1ba464b76e1f231b
Sven Pfleiderer <sg+github@roothausen.de>
2011-05-27 03:45:32 +0200

    Update parts unit tests

--- test/part.js
+++ test/part.js
@@ -3,12 +3,20 @@
  */
 
 var Assert = require('assert');
+var Async = require('async');
 var Store = require('../lib/store');
 var Part = require('../controller/part');
+
+var log = {
+  debug: console.log,
+  error: console.error
+};
+
 var context = {
   store: {
     parts: Store.createStore({ name: 'parts' })
-  }
+  }, 
+  log: log
 };
 var part = Part.part(context);
 var parts = Part.parts(context);
@@ -17,38 +25,40 @@ var headers = {
   'Content-Type': 'application/json'
 };
 
-exports['test /parts API'] = function (beforeExit) {
-
-  Assert.response(parts, {
-      url: '/',
-      timeout: 500
-    }, {
-      body: '{}',
-      status: 200,
-      headers: headers
+exports['test /parts API'] = function (done) {
+  context.store.parts.flush(function () {
+      Assert.response(parts, {
+          url: '/',
+          timeout: 500
+        }, {
+          status: 200,
+          body: '{}',
+          headers: headers
+        }, function () {
+          done();
+        });
     });
-
 };
 
-exports['get non existant part'] = function (beforeExit) {
-
+exports['get non existant part'] = function (done) {
   Assert.response(part, {
       url: '/IdontExist',
       timeout: 500
     }, {
       status: 404,
       headers: headers
+    }, function () {
+      done();
     });
 };
 
-exports['add new part and retrieve it'] = function (beforeExit) {
-
+exports['add new part and retrieve it'] = function (done) {
   Assert.response(part, {
       method: 'POST',
       url: '/',
       timeout: 500,
       headers: headers,
-      data: '{ "name": "asdf12" }'
+      data: JSON.stringify({ "name": "asdf12" })
     }, {
       body: /{"url":"\/part\/.*"}/,
       status: 200,
@@ -62,21 +72,23 @@ exports['add new part and retrieve it'] = function (beforeExit) {
           timeout: 500,
           headers: headers
         }, {
-          body: JSON.stringify({ "name": "asdf12" }),
           status: 200,
+          body: JSON.stringify({ "name": "asdf12" }),
           headers: headers
+        }, function (res) {
+          done();
         });
-    });
 
+    });
 };
 
-exports['retrieve non existant part property'] = function (beforeExit) {
+exports['get non existant property'] = function (done) {
   Assert.response(part, {
       method: 'POST',
       url: '/',
       timeout: 500,
       headers: headers,
-      data: '{ "name": "asdf12" }'
+      data: JSON.stringify({ "name": "asdf12" })
     }, {
       body: /{"url":"\/part\/.*"}/,
       status: 200,
@@ -86,66 +98,89 @@ exports['retrieve non existant part property'] = function (beforeExit) {
       var partId = JSON.parse(res.body).url.replace('/part/', '');
 
       Assert.response(part, {
-          url: '/' + partId + '/nonexistantprop', timeout: 500,
+          url: '/' + partId + '/doesnotexist',
+          timeout: 500,
           headers: headers
         }, {
           status: 404,
           headers: headers
+        }, function (res) {
+          done();
         });
 
     });
-
 };
 
+exports['add new properties to part'] = function (done) {
+  var partData = { name: 'asdf12', hurr: 'dürr' };
 
-exports['add new properties to part'] = function (beforeExit) {
-  var partData = { name: 'asdf12', hurr: 'durr' };
+  Async.waterfall([
 
-  Assert.response(part, {
-      method: 'POST',
-      url: '/',
-      timeout: 500,
-      headers: headers,
-      data: JSON.stringify(partData)
-    }, {
-      status: 200,
-      body: /{"url":"\/part\/.*"}/,
-      headers: headers
-    },
-    function (res) {
-      var partId = JSON.parse(res.body).url.replace('/part/', '');
+      function createPart(callback) {
+        Assert.response(part, {
+            method: 'POST',
+            url: '/',
+            timeout: 500,
+            headers: headers,
+            data: JSON.stringify(partData)
+          }, {
+            status: 200,
+            body: /{"url":"\/part\/.*"}/,
+            headers: headers
+          },
+          function (res) {
+            var partId = JSON.parse(res.body).url.replace('/part/', '');
+            callback(null, partId);
+          });
+      },
 
-      Assert.response(part, {
-          method: 'PUT',
-          url: '/' + partId + '/newprop',
-          timeout: 500,
-          data: '{"value":"asdf1234"}',
-          headers: headers
-        }, {
-          status: 200,
-          headers: headers
-        });
+      function setPartProperty(partId, callback) {
+        Assert.response(part, {
+            method: 'PUT',
+            url: '/' + partId + '/newprop',
+            timeout: 500,
+            data: JSON.stringify({ "value": "asdf1234" }),
+            headers: headers
+          }, {
+            status: 200,
+            headers: headers
+          }, function (res) {
+            callback(null, partId);
+          });
+      },
 
-      Assert.response(part, {
-          url: '/' + partId + '/newprop',
-          timeout: 500
-        }, {
-          status: 200,
-          body: '{"value":"asdf1234"}',
-          headers: headers
-        });
+      function getNewProperty(partId, callback) {
+        Assert.response(part, {
+            url: '/' + partId + '/newprop',
+            timeout: 500
+          }, {
+            status: 200,
+            body: JSON.stringify({ "value": "asdf1234" }),
+            headers: headers
+          }, function (res) {
+            callback(null, partId);
+          });
+      },
 
-      Assert.response(part, {
-          url: '/' + partId,
-          timeout: 500
-        }, {
-          status: 200,
-          headers: headers
-        }, function (res) {
-          partData.newprop = 'asdf1234';
-          Assert.deepEqual(JSON.parse(res.body), partData);
-        });
-
+      function validatePart(partId, callback) {
+        Assert.response(part, {
+            url: '/' + partId,
+            timeout: 500
+          }, {
+            status: 200,
+            headers: headers
+          }, function (res) {
+            partData.newprop = 'asdf1234';
+            Assert.deepEqual(JSON.parse(res.body), partData);
+            callback();
+          });
+      }
+    ], function () {
+      done();
     });
+};
 
+exports['tear down'] = function (done) {
+  context.store.parts.quit();
+  done();
 };
