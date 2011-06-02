c3f72af543c44a38ac73480dcf7d9c165aea1400
tv <tv@also>
2011-06-02 21:35:20 +0200

    controller/cart: use STORE-API

--- controller/cart.js
+++ controller/cart.js
@@ -2,8 +2,6 @@
 var connect = require('connect');
 var crypto = require('crypto');
 
-carts = {};
-
 exports.cart = function () {
   return connect(
       connect.router(function (app) {
@@ -11,70 +9,131 @@ exports.cart = function () {
         // create cart
         app.post('/', function(req, res) {
           var id = gensym();
-          carts[id] = {};
-          res.writeHead(200, {
-            'Content-Type': 'application/json'
+          app.store.carts.put(id, {}, function (err) {
+            if (err) {
+              res.writeHead(500, {
+                'Content-Type': 'application/json'
+              });
+              res.end('false');
+            } else {
+              res.writeHead(200, {
+                'Content-Type': 'application/json'
+              });
+              res.end('"/cart/' + id + '"');
+            };
           });
-          res.end('"/cart/' + id + '"');
         });
 
         // retrieve cart
         app.get('/(:id)', function(req, res) {
           var id = req.params.id;
-          if (id in carts) {
-            var cart = carts[id];
-            res.end(JSON.stringify(cart));
-          } else {
-            res.writeHead(404);
-            res.end("Not Found\n");
-          };
+          app.store.carts.exists(id, function (err, exists) {
+            if (err) {
+              res.writeHead(500, {
+                'Content-Type': 'application/json'
+              });
+              res.end('false');
+            } else {
+              if (exists) {
+                app.store.carts.get(id, function (err, item) {
+                  if (err) {
+                    res.writeHead(500, {
+                      'Content-Type': 'application/json'
+                    });
+                    res.end('false');
+                  } else {
+                    res.writeHead(200, {
+                      'Content-Type': 'application/json'
+                    });
+                    res.end(JSON.stringify(item));
+                  };
+                });
+              } else {
+                res.writeHead(404);
+                res.end("Not Found\n");
+              };
+            };
+          });
         });
 
         // add part to cart
         app.put('/(:cartId)/part/(:partId)', function(req, res) {
           var cartId = req.params.cartId;
           var partId = req.params.partId;
-          if (cartId in carts) {
-            carts[cartId][partId] = req.body;
-            // TODO 409
-            res.end();
-          } else {
-            res.writeHead(404);
-            res.end("Not Found\n");
-          };
+          // TODO 404 would be nice
+          app.store.carts.get(cartId, function (err, cart) {
+            if (err) {
+              res.writeHead(500, {
+                'Content-Type': 'application/json'
+              });
+              res.end('false');
+            } else {
+              cart[partId] = req.body;
+              app.store.carts.put(cartId, cart, function (err) {
+                if (err) {
+                  res.writeHead(500, {
+                    'Content-Type': 'application/json'
+                  });
+                  res.end('false');
+                } else {
+                  res.writeHead(200, {
+                    'Content-Type': 'application/json'
+                  });
+                  res.end('true');
+                };
+              });
+            };
+          });
         });
 
         // remove part from cart
         app.delete('/(:cartId)/part/(:partId)', function(req, res) {
           var cartId = req.params.cartId;
           var partId = req.params.partId;
-          if (cartId in carts) {
-            var cart = carts[cartId];
-            if (partId in cart) {
-              delete cart[partId];
-              res.end();
+          // TODO 404 would be nice
+          app.store.carts.get(cartId, function (err, cart) {
+            if (err) {
+              res.writeHead(500, {
+                'Content-Type': 'application/json'
+              });
+              res.end('false');
             } else {
-              res.writeHead(404);
-              res.end("Not Found\n");
+              delete cart[partId];
+              app.store.carts.put(cartId, cart, function (err) {
+                if (err) {
+                  res.writeHead(500, {
+                    'Content-Type': 'application/json'
+                  });
+                  res.end('false');
+                } else {
+                  res.writeHead(200, {
+                    'Content-Type': 'application/json'
+                  });
+                  res.end('true');
+                };
+              });
             };
-          } else {
-            res.writeHead(404);
-            res.end("Not Found\n");
-          };
+          });
         });
 
         // delete cart
         app.delete('/(:id)', function(req, res) {
           var id = req.params.id;
-          if (id in carts) {
-            delete carts[id];
-            res.end();
-          } else {
-            res.writeHead(404);
-            res.end("Not Found\n");
-          };
+          // TODO 404 would be nice
+          app.store.carts.del(id, function (err) {
+            if (err) {
+              res.writeHead(500, {
+                'Content-Type': 'application/json'
+              });
+              res.end('false');
+            } else {
+              res.writeHead(200, {
+                'Content-Type': 'application/json'
+              });
+              res.end('true');
+            };
+          });
         });
-
       })
   );
 };
