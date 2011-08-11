42094d5011468c24b0753cad80aed9bcf09393f0
makefu <github@syntax-fehler.de>
2011-08-11 23:33:02 +0200

    decrepel: initial commit

---
+++ tools/decrepel.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+sed -r '
+  # Gates Projection vs. crepel
+  1,3d;$d
+' |
+nl |
+sed -r '
+  s/=("[^"]*")/:\1,/g
+  s/<([^ ]+)/{ "type": "\1", /
+  s:/>:},:
+
+  s/^ *([0-9]+)/"\1":/
+' |
+sed -r '
+  1s/^/{\n/
+  $s/$/\n}/
+'
+
